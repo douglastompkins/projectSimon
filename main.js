77974e664f7e12ae8b864c3976a8d0663a744150
Douglas Tompkins <douglas_tompkins@hotmail.com>
2015-11-09 15:59:41 -0500

    lets go phil

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var blueSquare = document.getElementById('blue-box');
 var yellowSquare = document.getElementById('yellow-box');
 var greenSquare = document.getElementById('green-box');
 var startButton = document.getElementById('start-button');
-
+var resetButton = document.getElementById('restart-button');
 var simonColors = [redSquare, blueSquare, yellowSquare, greenSquare];
 
 var flashColor = function() {
@@ -24,11 +24,11 @@ var userMoves = [];
 var randomChoice = Math.random();
 
 var randomColor = function() {
-	if (randomChoice <= 0.25)
+	if (randomChoice < 0.26) {
 		return computerMove.push(redSquare);
-	} else if (randomChoice > 0.25 && <= 0.50) {
+	} else if (randomChoice > 0.25 && randomChoice < 0.51) {
 		return computerMove.push(blueSquare);
-	} else if (randomChoice > 0.50 && <= 0.75) {
+	} else if (randomChoice > 0.50 && randomChoice < 0.76) {
 		return computerMove.push(greenSquare);
 	} else {
         return computerMove.push(yellowSquare);
@@ -36,44 +36,26 @@ var randomColor = function() {
 
 };
 
-compareMoves = function(); {
-	if (computerMoves === userMoves)
-		return true;
-    } else {
-    	return false;
-    }
+// compareMoves = function(); {
+// 	if (computerMoves === userMoves)
+// 		return true;
+//     } else {
+//     	return false;
+//     }
 
-};
+// };
 
-var userInteraction = function(); {
-	if (redSquare.addEventListener('click', redSquare === true)
-		return userMoves.push(redSquare);
-	} else if (greenSquare.addEventListener('click', greenSquare === true) {
-		return userMoves.push(greenSquare);
-	} else if (yellowSquare.addEventListener('click', yellowSquare === true) {
-		return userMoves.push(yellowSquare);
-	} else if (blueSquare.addEventListener('click', blueSquare === true) {
-		return userMoves.push(blueSquare);
-	}
-};
-
-
-
-
-
-
-
-var target = event.target;
-event.target.classList.add(selector)
-event.target.removeEventListner(click, simonColors)
-}
-
-
-
-
-var newRound = function() {
-
-};
+// var userInteraction = function(); {
+// 	if (redSquare.addEventListener('click', redSquare === true)
+// 		return userMoves.push(redSquare);
+// 	} else if (greenSquare.addEventListener('click', greenSquare === true) {
+// 		return userMoves.push(greenSquare);
+// 	} else if (yellowSquare.addEventListener('click', yellowSquare === true) {
+// 		return userMoves.push(yellowSquare);
+// 	} else if (blueSquare.addEventListener('click', blueSquare === true) {
+// 		return userMoves.push(blueSquare);
+// 	}
+// };
 
 
 
@@ -81,23 +63,46 @@ var newRound = function() {
 
 
 
+// var target = event.target;
+// event.target.classList.add(selector)
+// event.target.removeEventListner(click, simonColors)
+// }
 
 
 
 
-var resetButton = document.getElementById('reset');
+// var newRound = function() {
+
+// };
+
+
+
+
 var clearGame = function(){
 	location.reload();
 };
 
+var startGameAlert = function() {
+	window.alert("The game Consists of 4 boxes containing diferrent colors, the computer selecs randomlly a color and is up to the user to remember and select the color. The computer will continue to select new Colors depending if the User sucessfully selects them.");
+}
 
-startButton.addEventListener('click', badMoveAlert);
 var badMoveAlert = function() {
-	alert ("Buuuh!! Wrong move, mate");
+	window.alert("Buuuh!! Wrong move, mate! Try Again!!");
 }; 
-resetButton.addEventListener('click', clearGame);
 
-redSquare.addEventListener('click', redSquare);
+var trialAlert = function() {
+	window.alert("do it");
+};
+
+
+
+
+
+startButton.addEventListener('click', startGameAlert);
+
+resetButton.addEventListener('click', badMoveAlert);
+
+redSquare.addEventListener('click', trialAlert);
 greenSquare.addEventListener('click', greenSquare);
 blueSquare.addEventListener('click', blueSquare);
 yellowSquare.addEventListener('click', yellowSquare);
